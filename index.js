a9cf55090a8897fee62e80aab38f6f003488d119
almahmud624 <mahmudbhuiyan624@gmail.com>
2022-10-31 03:49:00 +0600

    post product data

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 4000;
 
 const app = express();
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Blink Tech Connected");
 });
+
+const uri =
+  "mongodb+srv://blink-tech:iyMA9UIh0RtHtpOj@cluster0.4ieih.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const productCollection = client.db("blink-tech").collection("products");
+    // send data on server
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
+  } finally {
+    // client.close()
+  }
+}
+
+run().catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log("Blink Tech running on port", port);

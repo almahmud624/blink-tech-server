5d7ae6518ff9d7910428cdfd0177222ecbad50e5
almahmud624 <mahmudbhuiyan624@gmail.com>
2022-11-05 10:22:42 +0600

    jwt verfication added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
-
+const jwt = require("jsonwebtoken");
 // .env
 require("dotenv").config();
 
@@ -28,6 +28,23 @@ async function run() {
   try {
     const productCollection = client.db("blink-tech").collection("products");
     const ordersCollection = client.db("blink-tech").collection("orders");
+
+    // jwt verify function
+    const verifyJWT = (req, res, next) => {
+      const jwtHeaders = req.headers.authorization;
+      if (!jwtHeaders) {
+        return res.status(401).send({ message: "Unauthorized Access" });
+      }
+      const token = jwtHeaders.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(403).send("Forbidden Access");
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
     // send data on server
     app.post("/products", async (req, res) => {
       const product = req.body;
@@ -82,6 +99,17 @@ async function run() {
       res.send(result);
     });
 
+    // JWT
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
     // orders api
     // post orders
     app.post("/orders", async (req, res) => {
@@ -91,16 +119,35 @@ async function run() {
     });
 
     // get orders
-    app.get("/orders", async (req, res) => {
+    app.get("/orders", verifyJWT, async (req, res) => {
+      // valid email check
+      const decode = req.decoded;
+      if (decode.email !== req.query.email) {
+        res.status(403).send("Access Forbiden");
+      }
+
       let query = {};
+
+      // pagination
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+
+      // get data by specific email
       if (req.query.email) {
         query = {
           mail: req.query.email,
         };
       }
+
       const cursor = ordersCollection.find(query);
-      const orders = await cursor.toArray();
-      res.send(orders);
+      const orders = await cursor
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+
+      // set total data count for pagination
+      const count = await ordersCollection.estimatedDocumentCount();
+      res.send({ count, orders });
     });
 
     // order remove
@@ -111,7 +158,7 @@ async function run() {
       res.send(result);
     });
     // patch order
-    app.patch("/orders/:id", async (req, res) => {
+    app.patch("/orders/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const status = req.body.status;
